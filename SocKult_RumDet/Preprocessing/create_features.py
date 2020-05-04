ce4bbd1e04deb3d0a909c5b7fe401f0948b485a6
Malte Højmark-Bertelsen <malte@Maltes-MacBook-Pro.local>
2020-05-04 13:32:47 +0200

    added requirements.txt file
    Co-authored-by: Oliver Jarvis <oliversdatabase@gmail.com>

--- SocKult_RumDet/Preprocessing/create_features.py
+++ SocKult_RumDet/Preprocessing/create_features.py
@@ -1,12 +1,64 @@
 from SBERT_WK_Embedding import SBERT_WK_Embedding
 import pandas as pd
+from pandas_profiling import ProfileReport
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+import copy
 #from tree2branches import tree2branches
 
+# Loading the data
+source_tweets = "SocKult_RumDet/Preprocessing/data/twitter-english-source-clean-final.csv"
+reply_tweets = "SocKult_RumDet/Preprocessing/data/twitter-english-source-replies-clean-final.csv"
 
-source_tweets = "SocKult_RumDet/Preprocessing/data/twitter-english-source-clean.csv"
-reply_tweets = "SocKult_RumDet/Preprocessing/data/twitter-english-replies-clean.csv"
+source_df = pd.read_csv(source_tweets)
+reply_df = pd.read_csv(reply_tweets)
+datasets_decription = source_df.append(reply_df).describe()
 
 Embedder = SBERT_WK_Embedding()
+
+def correlation_heatmap(train):
+    correlations = train.corr()
+
+    fig, ax = plt.subplots(figsize=(10,10))
+    sns.heatmap(correlations, vmax=1.0, center=0, fmt='.2f',
+                square=True, linewidths=.5, annot=True, cbar_kws={"shrink": .70})
+    plt.show();
+
+def EDA(source_df, reply_df):
+    """function that does exploratory data analysis on the data"""
+
+    # Creating profile report
+    source_report = ProfileReport(source_df, title='Profile Report', html={'style':{'full_width':True}})
+    source_report.to_notebook_iframe()
+    source_report.to_file(output_file="EDA_source_report.html")
+
+    reply_report = ProfileReport(reply_df, title='Profile Report', html={'style':{'full_width':True}})
+    reply_report.to_notebook_iframe()
+    reply_report.to_file(output_file="EDA_reply_report.html")
+
+    correlation_heatmap(source_df)
+    correlation_heatmap(reply_df)
+
+    import pdfkit 
+    pdfkit.from_file('EDA_source_report.html', 'EDA_source_report.pdf') 
+    pdfkit.from_file('EDA_reply_report.html', 'EDA_reply_report.pdf') 
+
+def scale(value, col):
+    return (value - datasets_decription[col]['mean']) / datasets_decription[col]['std']
+
+def preprocess(row, feature_dict):
+    fd = copy.deepcopy(feature_dict)
+    #function that normalizes the features
+    for col in row:
+        if col == "user.verified":
+            if row[col] == "True":
+                fd[col] = 1
+            else:
+                fd[col] == 0
+        else:
+            fd[col] == scale(row[col], col)
+    return fd
 
 def metadata_for_tweet_id(tweet_id, tweet_type):
     source_file = ""
@@ -16,7 +68,7 @@ def metadata_for_tweet_id(tweet_id, tweet_type):
         source_file = pd.read_csv(reply_tweets, header=0)
 
     try:
-       row = source_file.loc[source_file['str_id'] == tweet_id]
+       row = source_file.loc[source_file['id_str'] == tweet_id]
     except:
         print("Row for str_id: [{}] missing from data".format(tweet_id))
         print("Unsure how to proceed.")    
@@ -28,7 +80,7 @@ def metadata_for_tweet_id(tweet_id, tweet_type):
         feature_dict[k] = v.values()[0]
     
     #preprocessing for meta data features
-    # 😃
+    feature_dict = preprocess(row, feature_dict)
 
     return feature_dict
 
@@ -39,7 +91,7 @@ def get_feature_vector(conversation):
     source_id = source['id_str']
     source_features = {}
     source_features['SBERT-WK'] = SBERT_WK_Embedding().get_embeddings(source_tweet)
-    #source_features['metadata'] = metadata_for_tweet_id(tweet_id=source_id, tweet_type=TweetTypes.source)
+    source_features['metadata'] = metadata_for_tweet_id(tweet_id=source_id, tweet_type="source")
     source_features['issource'] = 1
 
     fullthread_featdict = {}
@@ -48,8 +100,12 @@ def get_feature_vector(conversation):
         feature_dict = {}
         feature_dict['issource'] = 0
         #This is where the embeddings are added 
+        tweet_id = tw['id_str']
         feature_dict['SBERT-WK'] = Embedder.get_embeddings(tw['text'])
+        feature_dict['metadata'] = metadata_for_tweet_id(tweet_id=tweet_id, tweet_type="reply")
         fullthread_featdict[tw['id_str']] = feature_dict
+
+
 
     return fullthread_featdict
 
